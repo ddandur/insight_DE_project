1dfab7f985ce169b906ecb838e2d05664fb9052e
ddandur <daniel.r.dandurand@gmail.com>
2017-06-13 19:28:09 -0700

    removed unnecessary file from spark folder

--- spark/spark_stream_processor.py
+++ spark/spark_stream_processor.py
@@ -1,2 +1,16 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+# initialize stream
+sc = SparkContext(appName="NASA-logs")
+ssc = StreamingContext(sc, 1)
+# ssc.checkpoint("checkpoint") - leave out checkpointing for now
+
+# set up connection with kafka
+brokers = "ec2-52-42-160-109.us-west-2.compute.amazonaws.com" # master dns
+topic = 'NASA-logs'
+
+kafka_stream = KafkaUtils.createDirectStream(ssc,
+                                             [topic],
+                                             {"metadata.broker.list": brokers})
