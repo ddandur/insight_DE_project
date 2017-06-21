6d601e36cc0cd41788fb2baf745ae29bcf606302
ddandur <daniel.r.dandurand@gmail.com>
2017-06-20 20:15:02 -0700

    changed line in views.py

--- flask/app/views.py
+++ flask/app/views.py
@@ -14,10 +14,10 @@ redis_db = redis.StrictRedis(host=redis_server, port=6379, db=0)
 @app.route('/')
 @app.route('/index')
 def index():
-	
-	# pull sample data from redis server
-	pass
-	
+
+    # pull sample data from redis server
+    return "Hello, world!"
+
 """
    user = { 'nickname': 'Miguel' } # fake user
    array = [1,2,3,4]
@@ -29,18 +29,18 @@ def index():
 """
 @app.route('/api/<email>/<date>')
 def get_email(email, date):
-	stmt = "SELECT * FROM email WHERE id=%s and date=%s"
-	response = session.execute(stmt, parameters=[email, date])
+    stmt = "SELECT * FROM email WHERE id=%s and date=%s"
+    response = session.execute(stmt, parameters=[email, date])
         response_list = []
         for val in response:
              response_list.append(val)
-        jsonresponse = [{"first name": x.fname, "last name": x.lname, "id": x.id, "message": x.message, "time": x.time} for x in response_list] 
-	print jsonresponse
+        jsonresponse = [{"first name": x.fname, "last name": x.lname, "id": x.id, "message": x.message, "time": x.time} for x in response_list]
+    print jsonresponse
         return jsonify(emails=jsonresponse)
 
 @app.route('/email')
 def email():
-	return render_template("email.html")
+    return render_template("email.html")
 
 @app.route("/email", methods=['POST'])
 def email_post():
