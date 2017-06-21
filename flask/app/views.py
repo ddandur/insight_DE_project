221d659902367ad533e6c467d05b3ca88fcf02cb
Ubuntu <ubuntu@ip-10-0-0-14.us-west-2.compute.internal>
2017-06-21 03:11:18 +0000

    alterations to flask files

--- flask/app/views.py
+++ flask/app/views.py
@@ -4,16 +4,29 @@ from app import app
 from cassandra.cluster import Cluster
 
 # setting up connections to cassandra
-cluster = Cluster([os.environ["CASSANDRA_IP"]])
-session = cluster.connect('playground')
+# cluster = Cluster([os.environ["CASSANDRA_IP"]])
+# session = cluster.connect('playground')
+
+# connect to redis server
+redis_server = 'localhost'
+redis_db = redis.StrictRedis(host=redis_server, port=6379, db=0)
 
 @app.route('/')
 @app.route('/index')
 def index():
+	
+	# pull sample data from redis server
+	pass
+	
+"""
    user = { 'nickname': 'Miguel' } # fake user
    array = [1,2,3,4]
    return render_template("index.html", title = 'Home', user = user, array=array)
+"""
 
+
+
+"""
 @app.route('/api/<email>/<date>')
 def get_email(email, date):
 	stmt = "SELECT * FROM email WHERE id=%s and date=%s"
@@ -41,3 +54,4 @@ def email_post():
     jsonresponse = [{"fname": x.fname, "lname": x.lname, "id": x.id, "message": x.message, "time": x.time} for x in response_list]
     print jsonresponse
     return render_template("emailop.html", output=jsonresponse)
+"""
