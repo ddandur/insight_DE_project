f0e2b9aca5f3f9a74f6abe32a2851bd480d23d24
Ubuntu <ubuntu@ip-10-0-0-14.us-west-2.compute.internal>
2017-06-22 05:55:22 +0000

    files sends messages now

--- kafka/producer.py
+++ kafka/producer.py
@@ -14,16 +14,20 @@ class Producer(threading.Thread):
         topic = "NASA-logs" # change this later - these monitoring logs are mainly about latencies
         kafka_producer = KafkaProducer(bootstrap_servers='localhost:9092')
         for i in xrange(1000000):
+		message = """129.94.144.152 - - [01/Jul/1995:00:00:17 -0400] "GET /images/ksclogo-medium.gif HTTP/1.0" 304 0 {}""".format(i)
+		kafka_producer.send(topic, message)
+		time.sleep(.1)
+		print message + '\n' + 80*'=' + '\n' 
+		
             # create message with random latency drawn from lognormal(0,1) distribution
-            message = { 
-            			"id": "{}".format(i), 
-            			"latency": "{}".format(lognormal(1))
-                      }
-       
-            # message = """129.94.144.152 - - [01/Jul/1995:00:00:17 -0400] "GET /images/ksclogo-medium.gif HTTP/1.0" 304 0 {}""".format(i)
-            kafka_producer.send(topic, message)
-            time.sleep(.1)
-            print message + '\n' + 80*'=' + '\n'    	
+            #  message = { 
+            # 			"id": "{}".format(i), 
+            # 			"latency": "{}".format(lognormal(1))
+            #           }
+       	#  message = """129.94.144.152 - - [01/Jul/1995:00:00:17 -0400] "GET /images/ksclogo-medium.gif HTTP/1.0" 304 0 {}""".format(i)
+          #   kafka_producer.send(topic, message)
+           #  time.sleep(.1)
+           #  print message + '\n' + 80*'=' + '\n'    	
 
 
 if __name__ == "__main__":
